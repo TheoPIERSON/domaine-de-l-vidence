8be40d580a978f780350785e0bfa91af6e867e4d
Theo Pierson <thheopierson@gmail.com>
2025-03-28 17:12:32 +0100

    feat: stylize footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,38 +1,45 @@
 <template>
-  <footer class="flex">
-    <div>
+  <footer class="flex justify-center items-center flex-wrap p-10 font-montserrat">
+    <div class="w-screen font-cormorant text-4xl"><h2>Le domaine de l'évidence</h2></div>
+    <div class="m-4 p-4 border-r">
       <ul>
-        <li>Infos de contact</li>
-        <li>Adresse</li>
-        <li>Num tel</li>
-        <li>Adresse email</li>
+        <li class="font-bold">Infos de contact</li>
+        <li>Adresse: 1211 rue du Grand Chemin</li>
+        <li>Numéro de téléphone: 0629996475</li>
+        <li>Adresse email: ledomainedelevidence@gmail.com</li>
+      </ul>
+    </div>
+    <div class="m-4 p-4 border-r">
+      <ul>
+        <li class="font-bold">Lien utiles</li>
+        <li><NuxtLink to="/">Accueil</NuxtLink></li>
+        <li><NuxtLink to="/rooms">Logements</NuxtLink></li>
+        <li><NuxtLink to="#footer">Contact</NuxtLink></li>
+        <li><NuxtLink to="/">Mentions légales</NuxtLink></li>
+      </ul>
+    </div>
+    <div class="m-4 p-4 pr-8 border-r">
+      <ul>
+        <li class="font-bold">Réseaux sociaux</li>
+        <div class="flex w-full mt-4 justify-around items-center">
+          <a href="https://www.instagram.com/ledomainedelevidence/?hl=fr"
+            ><Icon class="p-3 hover:p-4 duration-200" name="formkit:instagram" style="color: black"
+          /></a>
+          <a href=""><Icon class="p-3 hover:p-4 duration-200" name="formkit:facebook" style="color: black" /></a>
+          <a href=""> <Icon class="p-3 hover:p-4 duration-200" name="formkit:tiktok" style="color: black" /> </a>
+        </div>
+      </ul>
+    </div>
+    <div class="m-4 p-4 border-r">
+      <ul>
+        <li class="font-bold">Site de réservation</li>
+        <li class="hover:underline"><a href="https://www.ledomainedelevidence.com/fr">Le domaine de l'évidence</a></li>
       </ul>
     </div>
     <div>
-      <ul>
-        <li>Lien utiles</li>
-        <li>accueil</li>
-        <li>logemnts</li>
-        <li>contact</li>
-        <li>mentions légales</li>
-      </ul>
-    </div>
-    <div>
-      <ul>
-        <li>Réseaux sociaux</li>
-        <li>Liens vers les réseaux</li>
-      </ul>
-    </div>
-    <div>
-      <ul>
-        <li>Site de réservation</li>
-        <li>Lien vers le site du domaine</li>
-      </ul>
-    </div>
-    <div>
-      <ul>
+      <ul class="font-bold">
         <li>crédits droit d'auteur</li>
-        <li>Exemple : "© 2025 Nom de la Maison d’Hôte. Tous droits réservés."</li>
+        <li>© 2025 Le Domaine de l'Evidence. Tous droits réservés.</li>
       </ul>
     </div>
   </footer>
